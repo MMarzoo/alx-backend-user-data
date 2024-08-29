f3f8847c95d38ab668540ba818b3e1c55165fff4
MMarzoo <mmarzouk12378@gmail.com>
2024-08-29 18:22:27 +0300

    adding filtered_logger.py file

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Module for handling Personal Data
+"""
+
+from typing import List
+import re
+import logging
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """
+    Replaces PII data with redacted data
+    """
+    for f in fields:
+        message = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
+    return message
