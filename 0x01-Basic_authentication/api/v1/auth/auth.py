5aec2a065376be8a336dd9e25011422a37bb0645
MMarzoo <mmarzouk12378@gmail.com>
2024-09-03 00:30:50 +0300

    adding files

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -37,6 +37,6 @@ class Auth():
             return None
         return request.headers.get('Authorization')
 
-    def current_user(self, request=None) -> TypeVar('User'):
+    def current_user(self, request=None) -> TypeVar('User'): # type: ignore
         """ Flask request object """
         return None
