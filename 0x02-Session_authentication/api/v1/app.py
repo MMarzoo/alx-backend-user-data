3f2876fecda79a7891d7f9a1cfc3d61cace9e28f
MMarzoo <mmarzouk12378@gmail.com>
2024-09-04 23:29:16 +0300

    adding files

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -4,6 +4,8 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
@@ -50,23 +52,21 @@ def before_request():
     """
     if auth is None:
         return
-    request_path_list = [
+
+    if auth.require_auth(request.path, [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
         '/api/v1/forbidden/'
-        ]
-    if not auth.require_auth(request.path, request_path_list):
+    ]) is False:
         return
 
-    if auth.authorization_header(request) is None \
-            and auth.session_cookie(request) is None:
+    if auth.authorization_header(request) is None:
         abort(401)
 
-    current_user = auth.current_user(request)
-    if current_user is None:
+    if auth.current_user(request) is None:
         abort(403)
 
-    request.current_user = current_user
+    request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -34,7 +34,7 @@ def view_one_user(user_id: str = None) -> str:
     if user_id == 'me' and request.current_user is not None:
         return jsonify(request.current_user.to_json())
 
-    user = user.get(user_id)
+    user = User.get(user_id)
     if user is None:
         abort(404)
     return jsonify(user.to_json())
