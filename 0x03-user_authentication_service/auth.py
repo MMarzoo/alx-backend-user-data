92162f860a1dd571f02ab26161a0fbb245b865aa
MMarzoo <mmarzouk12378@gmail.com>
2024-09-10 20:49:57 +0300

    adding files

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -56,3 +56,17 @@ class Auth:
             return checkpw(password.encode('utf-8'), user.hashed_password)
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """
+        find the user corresponding to the email, generate a new UUID
+        store it in the database as the users session_id
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+
+        session_id = _generate_uuid()
+        self._db.update_user(user.id, session_id=session_id)
+        return session_id
