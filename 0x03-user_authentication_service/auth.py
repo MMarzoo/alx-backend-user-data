974c02a3c30291d8adc4373f9982f13b5cb7404a
MMarzoo <mmarzouk12378@gmail.com>
2024-09-10 19:32:48 +0300

    adding files

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -16,11 +16,6 @@ def _hash_password(password: str) -> str:
     """
     return hashpw(password.encode('utf-8'), gensalt())
 
-def _generate_uuid() -> str:
-    """Generate a new UUID.
-    """
-    return str(uuid4())
-
 
 class Auth:
     """Auth class to interact with the authentication database.
@@ -36,8 +31,9 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-            raise ValueError(f'User ${email} already exists.')
+            raise ValueError(f'User {email} already exists')
         except NoResultFound:
-            hashed_password = _hash_password(password)
-            user = self._db.add_user(email, hashed_password)
+            hashed_pass = _hash_password(password)
+            user = self._db.add_user(email, hashed_pass)
+
             return user
