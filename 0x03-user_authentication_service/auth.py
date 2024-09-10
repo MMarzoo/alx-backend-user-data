5bb3493efc5895a0f434bba8166fb1da6f22b556
MMarzoo <mmarzouk12378@gmail.com>
2024-09-10 20:46:05 +0300

    adding files

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -17,6 +17,14 @@ def _hash_password(password: str) -> str:
     return hashpw(password.encode('utf-8'), gensalt())
 
 
+def _generate_uuid() -> str:
+    """
+    Returns string repr of a new UUID
+    Use uuid module
+    """
+    return str(uuid4())
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
@@ -37,3 +45,14 @@ class Auth:
             user = self._db.add_user(email, hashed_pass)
 
             return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        Expect email and password required arguments
+        Returns a boolean
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            return checkpw(password.encode('utf-8'), user.hashed_password)
+        except NoResultFound:
+            return False
