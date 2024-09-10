84ebbbb317d206b6d937915673924870d5bda764
MMarzoo <mmarzouk12378@gmail.com>
2024-09-10 19:29:10 +0300

    adding files

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -16,6 +16,11 @@ def _hash_password(password: str) -> str:
     """
     return hashpw(password.encode('utf-8'), gensalt())
 
+def _generate_uuid() -> str:
+    """Generate a new UUID.
+    """
+    return str(uuid4())
+
 
 class Auth:
     """Auth class to interact with the authentication database.
