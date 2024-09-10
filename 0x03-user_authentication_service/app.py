b10353b7b7f4cad1281c4417d19e2cefaec0d756
MMarzoo <mmarzouk12378@gmail.com>
2024-09-10 20:29:20 +0300

    adding files

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -21,5 +21,24 @@ def welcome() -> str:
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route('/auth/register', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """
+    POST /users, JSON: -email, -password
+    Returns end-point to register a user
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user = AUTH.register_user(email, password)
+        if user is not None:
+            return jsonify({
+                "email": user.email,
+                "message": "user created"
+            })
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
