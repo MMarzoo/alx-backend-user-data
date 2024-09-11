cd3005cc5212e38fb60b2521c54580321aba468c
MMarzoo <mmarzouk12378@gmail.com>
2024-09-11 23:23:48 +0300

    adding files

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -121,5 +121,6 @@ class Auth:
         except NoResultFound:
             raise ValueError
         hashed_password = _hash_password(password)
-        self._db.update_user(user.id, password=hashed_password,
+        self._db.update_user(user.id,
+                             hashed_passwordpassword=hashed_password,
                              reset_token=None)
