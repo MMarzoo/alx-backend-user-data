b8b55e1357d4c33f343458febcb019f9e3c9a447
MMarzoo <mmarzouk12378@gmail.com>
2024-09-11 22:20:50 +0300

    adding files

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def welcome() -> str:
-    """GET /
+    """
     Returns welcome message
     """
     return jsonify({"message": "Bienvenue"})
@@ -24,7 +24,6 @@ def welcome() -> str:
 @app.route('/users', methods=['POST'], strict_slashes=False)
 def users() -> str:
     """
-    POST /users, JSON: -email, -password
     Returns end-point to register a user
     """
     email = request.form.get('email')
@@ -34,6 +33,24 @@ def users() -> str:
         return jsonify({"email": email, "message": "user created"})
     except Exception:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """
+    Returns request with form data with email and password fields
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    valid_log = AUTH.valid_login(email, password)
+
+    if not valid_log:
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie('session_id', session_id)
+    return response
 
 
 if __name__ == "__main__":
